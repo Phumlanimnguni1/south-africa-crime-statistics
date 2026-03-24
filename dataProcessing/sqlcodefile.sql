153033052731b6cfe1bc98521287fd7a3e512eff
phumlanimnguni1 <prosperproper700@gmail.com>
2026-03-24 19:35:47 +0200

    did sql queries including record retrieval, counting cases, filtering and sorting

--- dataProcessing/sqlcodefile.sql
+++ dataProcessing/sqlcodefile.sql
@@ -1,36 +1,51 @@
-select top 10 *
-from [saps_crime_data].[dbo].[saps_crime_data]
-
---South Africa Criminal Database - 30 SQL Questions (Questions Only)
+--South Africa Criminal Database  
 --1. Display all records from the CriminalCases table.
-
+select *
+from [saps_crime_data].[dbo].[saps_crime_data];
 
 --2. Count the total number of cases in the table.
-
+select count(casenumber) as Total_number_of_cases
+from [saps_crime_data].[dbo].[saps_crime_data];
 
 --3. Count the number of cases in each province.
-
+select province,count(casenumber) as number_of_cases
+from [saps_crime_data].[dbo].[saps_crime_data]
+group by province
+order by number_of_cases desc;
 
 --4. Retrieve all cases where the crime type is Fraud.
-
+select *
+from [saps_crime_data].[dbo].[saps_crime_data]
+where CrimeType = 'fraud';
 
 --5. Count the number of cases for each crime type.
-
+select CrimeType,count(casenumber) as number_of_cases
+from [saps_crime_data].[dbo].[saps_crime_data]
+group by CrimeType
+order by number_of_cases desc;
 
 --6. Display all cases classified as High or Critical risk.
-
+select *
+from [saps_crime_data].[dbo].[saps_crime_data]
+where RiskLevel in ('high','critical');
 
 --7. Calculate the average Financial Score of all suspects.
-
+select avg(financialscore) as Average_Financial_Score
+from [saps_crime_data].[dbo].[saps_crime_data];
 
 --8. Find the highest Estimated Fraud Amount recorded.
-
+select MAX(estimatedfraudamount_zar) as Highest_Fraud_Amount
+from [saps_crime_data].[dbo].[saps_crime_data];
 
 --9. Retrieve the top 10 cases with the highest Estimated Fraud Amount.
-
+select top 10 *
+from [saps_crime_data].[dbo].[saps_crime_data]
+order by EstimatedFraudAmount_ZAR desc;
 
 --10. Display all cases where the suspect was arrested.
-
+select  *
+from [saps_crime_data].[dbo].[saps_crime_data]
+where arrested = 'yes';
 
 --11. Count how many suspects were arrested versus not arrested.
 
@@ -91,3 +106,10 @@ from [saps_crime_data].[dbo].[saps_crime_data]
 
 --30. Identify the most common Crime Type in the dataset.
 
+
+--31. Write a query to show the number of high-risk and critical SAPS cases, broken down by Gender and CrimeType. Display the results with the highest number of cases first.
+select gender,  crimetype, count(casenumber) as number_cases
+from [saps_crime_data].[dbo].[saps_crime_data]
+where RiskLevel in ('high','critical')
+group by gender, crimetype
+order by number_cases desc;
